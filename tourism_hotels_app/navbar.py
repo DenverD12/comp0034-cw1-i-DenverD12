5e7ef3601536929d8ccbed026e320a51081fed3f
Denver D'Silva <denverd245@gmail.com>
2023-02-14 03:05:56 +0000

    fix: updated navbar and main app  to meet PEP-8

---
+++ tourism_hotels_app/navbar.py
@@ -0,0 +1,92 @@
+"""Helper file with bootstrap navbar function."""
+from dash import html
+import dash_bootstrap_components as dbc
+
+CUSTOM_LOGO = "https://static.thenounproject.com/noun-svg/41150.svg?Expires=1676221204&Signature=jfIz73qyjmFSVqaaTJ~Tu~XfYxjm-87Cl~cx8fMSvuc8HYKeCDpBUL1gqA3ZSgGiM2XcpoofB76pBDdMf15dUJqWeJrPx-gTuWyA1KsW6TSie1eSfFj14hPHTpka12y6W4wLCKvueaogJHdR5Hn3QqQ4XPywN2OqTPbJkpVxz40_&Key-Pair-Id=APKAI5ZVHAXN65CHVU2Q"
+
+
+# Define navigation bar function
+def Navbar():
+    # Define search bar in a bootstrap container
+    search_bar = dbc.Container(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(
+                        # Add input field to type search queries
+                        [dbc.Input(type="search", placeholder="Search")],
+                        # Format spacing to neatly fit in the navbar band
+                        className="ms-auto flex-nowrap mt-3 mt-md-0 gx-2",
+                    ),
+                    dbc.Col(
+                        [  # Add button to submit the search
+                            dbc.Button(
+                                "Search", color="primary", className="ms-2", n_clicks=0
+                            )
+                        ],
+                        width="auto",
+                        className="ms-auto flex-nowrap mt-3 mt-md-0 border rounded px-2 ps-0 me-3 gx-4",
+                    ),
+                ],
+                # Increase horizontal gutter spacing
+                className="gx-1",
+            ),
+        ]
+    )
+    # Define dash bootstrap navbar
+    navbar = dbc.Navbar(
+        dbc.Container(
+            [
+                html.A(
+                    # Use row and column to control vertical alignment of logo
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Img(src=CUSTOM_LOGO, height="30px")),
+                            dbc.Col(
+                                dbc.NavbarBrand(
+                                    "TOURISM & HOSPITALITY", className="ms-2"
+                                )
+                            ),
+                        ],
+                        align="center",
+                        className="g-0",
+                    ),
+                    # Add a hyperlink to the first (home) page "pg1"
+                    href="/",
+                    style={"textDecoration": "none"},
+                ),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                dbc.Nav(
+                                    [  # Add 3 navigation bar link objects with a link to each page
+                                        dbc.NavItem([dbc.NavLink("Home", href="/")]),
+                                        dbc.NavItem(dbc.NavLink("Trends", href="/pg2")),
+                                        dbc.NavItem(dbc.NavLink("Posts", href="/pg3")),
+                                    ]
+                                )
+                            ],
+                            className="px-4 fw-bold gx-4",
+                        )
+                    ]
+                ),
+                # Toggler for navbar to collapse on small screens
+                # Connected to the callback in main file with id
+                dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+                dbc.Collapse(
+                    search_bar,
+                    id="navbar-collapse",
+                    is_open=False,
+                    navbar=True,
+                ),
+            ],
+            # Set fluid to fill width of screen on all screen size
+            fluid=True,
+        ),
+        # Change color of navbar to blue
+        color="primary",
+        # Make navbar text white for better visual
+        dark=True,
+    )
+    return navbar
--- tourism_hotels_app/tourism_hotels_dash_app.py
+++ tourism_hotels_app/tourism_hotels_dash_app.py
@@ -1,62 +1,63 @@
-"""Main dash app."""
+"""Main dash app layout that links the multiple pages."""
 import dash
-from dash import html, dcc, Dash, dash_table, Input, Output, State
+from dash import html, dcc, Dash, Input, Output, State
 import dash_bootstrap_components as dbc
-from pages.navbar import Navbar
+from navbar import Navbar
 
 
 # Create the dash app
-app = Dash(__name__,
-           # Use bootstrap css style sheet
-           external_stylesheets=[dbc.themes.BOOTSTRAP],
-           meta_tags=[
-            # 
-            {"name": "viewport", "content": "width=device-width, initial-scale=1"},
-           ],
-           use_pages=True
-           )
+app = Dash(
+    __name__,
+    # Use bootstrap css style sheet
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+    ],
+    use_pages=True,
+)
 
 # Wrap content of the dash app in a responsive width dbc container
 app.layout = dbc.Container(
     # Set fluid to true to make container fill entire screen width
     fluid=True,
-    children=[   
+    class_name="bg-light",
+    children=[
         Navbar(),
         # Blank line / line break
         html.Br(),
-
-        # First row here
-        ## Using dbc bootstrap to split page into rows and columns
-        dbc.Row([
+        # First row here add the title
+        dbc.Row(
             dbc.Col(
-                html.H1("Tourism"),
-                width="auto")],
-                justify="center",
-                ),
-
-        dbc.Row([
+                [
+                    html.H1(["International Tourism:"], className="text-dark"),
+                ],
+                width="auto",
+            ),
+            justify="center",
+        ),
+        # Second row add lead text
+        dbc.Row(
             dbc.Col(
                 html.H2(
-                     "International Tourist Arrivals",
-                     className='lead'
-                    ),
-                width="auto")],
-                justify="center",
+                    ["Find the Best Destinations for Hospitality Development"],
+                    className="lead text-dark",
                 ),
-
-        # Blank line / line break
+                width="auto",
+            ),
+            justify="center",
+        ),
+        # Add line break
         html.Br(),
-
-        dbc.Row([
+        # Add multi-page contents container
+        dbc.Row(
             dbc.Col(
                 dash.page_container,
-                )],
-                justify="center",
-                ),
-        
-            
+            ),
+            justify="center",
+        ),
     ],
-    )
+)
+
 
 # add callback for toggling the search bar collapse on small screens
 @app.callback(
@@ -69,5 +70,6 @@ def toggle_navbar_collapse(n, is_open):
         return not is_open
     return is_open
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8051)
